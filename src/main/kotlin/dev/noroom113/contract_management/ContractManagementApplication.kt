567db2c6ad29dc05ecb32c3f9bf2b99b00d9cd79
thuanpt <tienthuan05082002@gmail.com>
2024-04-08 11:44:45 +0700

    Initial commit

---
+++ src/main/kotlin/dev/noroom113/contract_management/ContractManagementApplication.kt
@@ -0,0 +1,11 @@
+package dev.noroom113.contract_management
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ContractManagementApplication
+
+fun main(args: Array<String>) {
+    runApplication<ContractManagementApplication>(*args)
+}
