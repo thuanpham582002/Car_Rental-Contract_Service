03c24beeef5f6e141674793680d98c03c621789c
thuanpt <tienthuan05082002@gmail.com>
2024-04-22 07:23:18 +0700

    idk what am i to do

--- src/main/kotlin/dev/noroom113/contract_management/ContractServiceApplication.kt
+++ src/main/kotlin/dev/noroom113/contract_management/ContractServiceApplication.kt
@@ -2,8 +2,12 @@ package dev.noroom113.contract_management
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableScheduling
 class ContractManagementApplication
 
 fun main(args: Array<String>) {
